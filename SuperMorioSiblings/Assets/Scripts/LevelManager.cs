f2ec802af077a6c22ff82c190b0b0558ea442493
Miquel Nadal <miquelnadalmudarra@gmail.com>
2024-05-28 10:58:54 +0200

    Basic Score UI

--- SuperMorioSiblings/Assets/Scripts/LevelManager.cs
+++ SuperMorioSiblings/Assets/Scripts/LevelManager.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelManager : MonoBehaviour
 {
 
     private int points = 0;
+
+    //Score UI is handled in the level manager because it's pretty simple and there's no need  to make a manager for it right now
+    [SerializeField]
+    public Text scoreText;
 
     //Points to win, settable through Inspector
     [SerializeField]
@@ -18,6 +24,8 @@ public class LevelManager : MonoBehaviour
         {
             restartLevel();
         }
+  
+        scoreText.text = "Snacks remaining: " + (wincond - points).ToString();
     }
 
     public void restartLevel()
